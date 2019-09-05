92a88bdaf8d19d51da47257cbe7e2fc968324af0
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-09-05 12:41:59 -0400

    reorged file structure, now getting six blank cards, but still six cards....

--- clickmarn/src/App.js
+++ clickmarn/src/App.js
@@ -1,25 +1,32 @@
 import React from "react";
+import Wrapper from "./components/Wrapper";
 import Navbar from "./components/Navbar";
-import Main from "./components/Main"
+import Cards from "./components/Cards";
+import pics from "./pics.json";
 
-const styles = {
-  container: {
-    background: "#FFB6C1"
+class App extends React.Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    pics
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <div className="container">
+          {this.state.pics.map(pic => (
+            <Cards
+              // removeFriend={this.removeFriend}
+              id={pic.id}
+              key={pic.id}
+              image={pic.image}
+            />
+          ))}
+        </div>
+      </Wrapper>
+    );
   }
-};
-
-function App() {
-  return (
-    <div>
-      <div style={styles.container} className="container">
-        <Navbar />
-      </div>
-
-      <div className="main">
-        <Main /> 
-      </div>
-    </div>
-  );
 }
 
 export default App;
