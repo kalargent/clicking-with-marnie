9fb9aa65a77b973249f77f32837a66da2cca9b8c
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-09-05 17:40:31 -0400

    minor update to Navbar

--- clickmarn/src/components/Footer/index.js
+++ clickmarn/src/components/Footer/index.js
@@ -4,9 +4,6 @@ import "./style.css";
 function Footer () {
     return (
         <footer className="page-footer">
-        <div className="container">
-          
-        </div>
         <div className="footer-copyright">
           <div className="container">
           Copyright © 2019 by Karen Largent 
