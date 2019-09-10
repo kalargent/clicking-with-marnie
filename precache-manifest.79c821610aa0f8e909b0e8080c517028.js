f542b72f71b65bae6e3b7a2a99667028ebcb944a
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-09-09 21:49:10 -0400

    Updates

---
+++ precache-manifest.79c821610aa0f8e909b0e8080c517028.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a4b27c8d356524b21b5c1d99e0747f",
+    "url": "/clicking-with-marnie/index.html"
+  },
+  {
+    "revision": "7aee1ac3fc166cefe4ff",
+    "url": "/clicking-with-marnie/static/css/main.78a04103.chunk.css"
+  },
+  {
+    "revision": "d679830f25046908d563",
+    "url": "/clicking-with-marnie/static/js/2.955e364c.chunk.js"
+  },
+  {
+    "revision": "7aee1ac3fc166cefe4ff",
+    "url": "/clicking-with-marnie/static/js/main.df60ce38.chunk.js"
+  },
+  {
+    "revision": "a25506e842fab5d95587",
+    "url": "/clicking-with-marnie/static/js/runtime~main.44a21774.js"
+  }
+]);
